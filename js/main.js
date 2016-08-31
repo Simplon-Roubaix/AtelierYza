fc30fbadb3563c6e05b99843a2cb814b73bfcb9d
NTUMBA <cityzenmboma@yahoo.fr>
2016-08-31 14:37:52 +0200

    Inside index file:I've add pictures, 4 cards (for materialize framework), html form and a table.In main js 've added code for random image. main css i've insert an style for random picture.Into gallery file nothing change

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-
+var random_images_array = ["20160820_194807_Richtone(HDR).jpg", "Appart_balcons.JPG", "architecte.JPG", "avec_garage.JPG", "charlott lingerie.jpg", "eco_compatible.JPG", "fresnoy.JPG", "machines.jpg", "maison_bleue.JPG", "maison_tradition.JPG", "moderne.JPG", "quartier_paisible.JPG", "siege.JPG", "style_alsace.JPG", "traditionnel.JPG"];
+function getRandomImage(imgAr, path) {
+    path = path || 'img/img_random'; // default path here
+    var num = Math.floor( Math.random() * imgAr.length );
+    var img = imgAr[ num ];
+    var imgStr = '<img src="img/img_random' + path + img + '" alt = "atelierdYza">';
+    document.write(imgStr); document.close();
+}
