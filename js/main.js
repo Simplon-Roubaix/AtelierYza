ac886809335b2d26e2ae1b184968ea7573325022
NTUMBA <cityzenmboma@yahoo.fr>
2016-11-15 17:39:19 +0100

    Index.html: j'ai modifié les paramètres et la position du caroussel. J'ai placé le paragraphe dans un card et ajouté un titre. Fichier espace libre service: ajout d'un lien. Page contact ajout checkbox et texte en français avec ajout de background. Changer couleur des liens

--- js/main.js
+++ js/main.js
@@ -6,6 +6,14 @@ $(document).ready(function(){
 
 $(document).ready(function(){
           $('.carousel').carousel();
-              
+          $('.carousel').carousel({
+    padding: 200
 });
+autoplay()
+function autoplay() {
+    $('.carousel').carousel('next');
+    setTimeout(autoplay, 4500);
+}
 
+
+});
