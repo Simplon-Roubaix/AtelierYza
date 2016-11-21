9a3bf1eca6c455a7cf19127cc6d6bad1e55870d6
NTUMBA <cityzenmboma@yahoo.fr>
2016-11-21 13:27:56 +0100

    Suppression de ligne js pour éviter conflict

--- js/main.js
+++ js/main.js
@@ -18,26 +18,3 @@ function autoplay() {
 
 
 });
-
-
-function cycleImages(){
-      var $active = $('.anim_concept .active');
-      var $next = ($active.next().length > 0) ? $active.next() : $('.anim_concept img:first');
-      // Pour déplacer l'image suivante vers le premier plan
-      $next.css('z-index',2);
-
-      // Décoloré l'image située au premier plan
-      $active.fadeOut(1500,function(){
-
-      // Réinitialiser le z-index et afficher l'image
-	  $active.css('z-index',1).show().removeClass('active');
-
-      //  Make the next image the top one
-          $next.css('z-index',3).addClass('active');
-      });
-    }
-
-$(document).ready(function(){
-// run every 7s
-setInterval('cycleImages()', 5000);
-})
